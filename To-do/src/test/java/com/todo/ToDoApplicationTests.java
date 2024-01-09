ac364c19afd0a9035e528cd49303b3a20ba44647
CoffeerLatte <125641153+CoffeerLatte@users.noreply.github.com>
2024-01-09 10:19:45 +0900

    Todo First commit/push

--- To-do/src/test/java/com/todo/ToDoApplicationTests.java
+++ To-do/src/test/java/com/todo/ToDoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.test;
+package com.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class ToDoApplicationTests {
 
 	@Test
 	void contextLoads() {
